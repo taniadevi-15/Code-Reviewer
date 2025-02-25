f768f73420369277990afe037bf5e69615ab969c
tania <taniarajput303@gmail.com>
2025-02-25 10:32:17 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const aiRoutes = require('./routes/ai.routes')
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+ app.use('/ai',aiRoutes)
+module.exports = app
